c12bfa19d43dc8ed772391d6bd5f01d448261556
Miguel <galleriesproductions@gmail.com>
2025-03-31 22:21:56 -0400

    Render Host Problems

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   envDir: './environment',
   server: {
     host: '0.0.0.0', // Allow connections from any host
+    allowedHosts: ['candidate-search-kx1t.onrender.com'], // Allow connections from this host
     port: Number(process.env.PORT) || 5173, // Convert process.env.PORT to a number, fallback to 5173 if undefined or invalid
   },
 });
